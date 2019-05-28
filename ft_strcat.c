1b219fe556233946b43df437b6961834812c566c
Mulunghisi NUKERI <mnukeri@e5r10p9.wethinkcode.co.za>
2019-05-28 17:25:36 +0200

    string cat update

---
+++ ft_strcat.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char	*ft_strcat(char *s1, const char *s2)
+{
+	int p, k;
+
+	p = 0;
+	while (s1[p] != '\0')
+	{
+		p++;
+	}
+	k = 0;
+	while (s2[k] != '\0')
+	{
+		s1[p + k] = s2[k];
+		k++;
+	}
+	s1[p + k]  = '\0';
+	return (s1);
+}
