862f96ba5036869b823e5ef12f5eaa061f078f94
Mulunghisi NUKERI <mnukeri@c5r10s9.wethinkcode.co.za>
2019-06-02 15:11:23 +0200

    string r chr update

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -5,7 +5,7 @@ char	*ft_strrchr(const char *s, int c)
 	int p;
 
 	p = sizeof(s);
-	while (s[p] != s[0])
+	while (p >= 0)
 	{
 		if (s[p] == c)
 		{
@@ -13,17 +13,8 @@ char	*ft_strrchr(const char *s, int c)
 		}
 		else
 		{
-			p--;
+		p--;
 		}
 	}
-	return (0);
-}
-
-int	main()
-{
-	char str[] = "it got to a point, when i decided, i can't do this anymore";
-
-	printf("%s\n", ft_strrchr(str, 100));
-	printf("%s", strrchr(str, 100));
 	return (0);
 }
