5cacaf842a4a8a12376cbf33eda7dd039c4db620
Mulunghisi NUKERI <mnukeri@c5r10s9.wethinkcode.co.za>
2019-06-05 16:05:11 +0200

    memmove update

---
+++ ft_memmove.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 15:50:23 by mnukeri           #+#    #+#             */
+/*   Updated: 2019/06/05 16:04:01 by mnukeri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, void *src, size_t len)
+{
+	size_t			p;
+	unsigned char	*st;
+	unsigned char	*s;
+
+	st = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	p = 0;
+	while (p < len)
+	{
+		st[p] = s[p];
+		p++;
+	}
+	return (dest);
+}
