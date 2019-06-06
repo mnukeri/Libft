f5315b32914d806e3c392e983e8948a5ccc7fae7
Mulunghisi NUKERI <mnukeri@c5r10s9.wethinkcode.co.za>
2019-06-06 17:52:32 +0200

    isprint update

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 18:15:49 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/03 19:01:38 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/06 17:52:06 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,7 @@
 
 int		ft_isprint(int c)
 {
-		int p;
-
-		p = 0;
-		while (p < 127)
-		{
-			if (isprint(p) == 0)
-			{
-				write(1, c, 1);
-				p++;
-			}
-			else
-			{
-				p++;
-			}
-		}
-}
-
-int		main()
-{
-	//printf("%d\n", ft_isprint('#'));
-	printf("%d\n", isprint(43));
-	printf("%c", 43);
+	if (c >= 32 && c <= 126)
+		return (1);
 	return (0);
 }
