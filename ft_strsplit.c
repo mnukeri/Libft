528d211aa548f42cd9491493424feeb8ebda2170
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-06-11 18:35:45 +0200

    string split update

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,59 +6,81 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 12:41:00 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/10 17:59:56 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/11 18:34:45 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_strspilt(char const *str, char c)
+char	ft_malloc(int k, int p)
 {
-	char *st;
+	int m;
+
+	m = 0;
+	while (m < k)
+	{
+		new[m] = (char*)malloc(p * sizeof(char));
+		if (new[m] == NULL)
+			return (NULL);
+		m++;
+	}
+	new[m] = '\0';
+	return (new);
+}
+
+int		ft_wc(char *str, int c)
+{
 	int p;
 	int k;
-	int i;
 
 	p = 0;
 	k = 0;
-	st = (char**)malloc(sizeof(char) * (ft_strlen(src) + 20));
 	while (str[p] != '\0')
-	{
 		if (str[p] == c)
-		{
-			p++;
-		}
-		else
-		{
-			while (str[p] != c)
-			{
-				st[k][i] = str[p];
-				p++;
-				i++;
-			}
 			k++;
-		}
-	}
-	st[k] = '\0';
-	return (st);
+		p++;
+	return (k);
 }
 
-int 	main()//int argc, char **argv)
+char	**ft_strsplit(char const *str, char c)
+{
+	int p;
+	int k;
+	int m;
+	char *st;
+	char **new;
+
+	st = (char*)str;
+	k = ft_wc(st, c);
+	new = (char **)malloc((k + 2) * sizeof(char *));
+	if (new == NULL)
+		return (NULL);
+		
+	p = 0;
+	m = 0;
+	while (st[p] != '\0')
+	{
+		k = 0;
+		while (st[p] != ' ')
+		{
+			new[m][k] = st[p];
+			p++;
+			k++;
+		}
+		m++;
+	}
+	new[m][1] = '\0';
+	return ((char **)new);
+}
+
+int 	main()
 {
 	
-	char **stp = "cause lately i have been a lil fed up, wish you would just";
-	char **str;
-	
-	str = ft_strsplit(stp[1], ' ');
-	printf("%s\n", stp);
-	printf("%s", *str);
-	/*
-	int i = 0;
+	char stp[] = "cause lately i have been a lil fed up, wish you would just";
+	char **s;
 
-	if (argc < 1)
-		return (0);
-	while (i < 2)
-		ft_strsplit(&argv[1][1], ' ');
-	*/
+	s = ft_strsplit(stp, ' ');	
+	printf("%s\n", s[1]);
+	printf("%s", stp);
 	return (0);
 }
