b2cde268557562b2954b8df92ccb172f3aa1eec7
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-06-12 17:38:52 +0200

    libft.h update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 14:02:38 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/11 18:12:55 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/12 17:29:44 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,5 +56,7 @@ char	**ft_strsplit(char const *str, char c);
 char	*ft_strtrim(char const *s);
 void	ft_putendl(char const *s);
 char	*ft_strnew(size_t size);
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
 
 #endif
