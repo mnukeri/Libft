395ada66a985139a1a82c45b8b236b7271adbf48
Mulunghisi NUKERI <mnukeri@c5r7s2.wethinkcode.co.za>
2019-06-21 14:42:18 +0200

    strsplit update

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,64 +6,37 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 12:41:00 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/20 17:02:53 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/21 14:42:01 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-//#include "libft.h"
-/*
-static int		ft_rows(const char *str, char c)
-{
-	int k;
-	int p;
+#include "libft.h"
 
-	k = 0;
-	p = 0;
-	while (str[k] != '\0')
-	{
-		if (str[k] == c)
-			k++;
-		p++;
-	}
-	return (k + 1);
-}
-*/
 char	**ft_strsplit(char const *str, char c)
 {
 	int		p;
 	int		i;
 	int		j;
-	//int		rows;
+	int		rows;
 	char	**y;
 
-	//rows = ft_rows(str, c);
-	//printf("%d\n", rows);
+	rows = ft_wc(str);
+	printf("%d\n", rows);
 	y = (char**)malloc(sizeof(char*) * 11);
 	if (y == NULL)
 		return (NULL);
-	/*i = 0;
-	while (i < 10)
-	{
-		y[i] = (char*)malloc(sizeof(char) * 10);
-		if (y[i] == NULL)
-			return (NULL);
-		i++;
-	}*/
 	i = 0;
-	j = 0;
 	p = 0;
 	while (str[p] != '\0' && i < 10)
 	{
 		while (str[p] == c && str[p])
-		{
 			p++;
-		}
-		
 		if (str[p])
 		{
-			y[i] = (char *)malloc(sizeof(char) * 10);
+			y[i] = (char*)malloc(sizeof(char) * 10);
+			if (y[i] == NULL)
+				return (NULL);
+			j = 0;
 			while (str[p] && str[p] != c)
 			{
 				y[i][j] = str[p];
@@ -71,7 +44,6 @@ char	**ft_strsplit(char const *str, char c)
 				p++;
 			}
 			y[i][j] = '\0';
-			j = 0;
 		}
 		i++;
 	}
