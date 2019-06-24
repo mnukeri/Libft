5ce035b3aa0ea598fbb68e4a34651e5d5af5236e
Mulunghisi NUKERI <mnukeri@c5r8s5.wethinkcode.co.za>
2019-06-24 18:39:39 +0200

    string split update

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,12 +6,12 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 12:41:00 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/24 17:49:57 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/24 18:39:13 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <ctype.h>
+#include <stdio.h>
 
 char	**ft_strsplit(char const *str, char c)
 {
@@ -21,9 +21,9 @@ char	**ft_strsplit(char const *str, char c)
 	int		rows;
 	char	**y;
 
-	if (!(y = (char**)malloc(sizeof(char*) * (ft_wc(str, c)))))
-		return (NULL);
 	rows = ft_wc(str, c);
+	if (!(y = (char**)malloc(sizeof(char*) * rows)))
+		return (NULL);
 	i = 0;
 	p = 0;
 	while (str[p] && i < rows)
@@ -40,22 +40,4 @@ char	**ft_strsplit(char const *str, char c)
 	}
 	y[i] = NULL;
 	return (y);
-}
-
-int		main(void)
-{
-	char stp[] = "bu the  iyou  ";// keys be playing hard , all 4our of 'em  ";
-	char **cl;
-	int		m;
-	int rows;
-
-	rows = ft_wc(stp, ' ');
-	printf("rows: %d\n", rows);
-	cl = ft_strsplit(stp, ' ');
-	m = 0;
-	while (cl[m])
-	{
-		printf("%s\n", cl[m++]);
-	}
-	return (0);
 }
