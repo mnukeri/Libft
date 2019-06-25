bdfb7bfabf09ff95bd6140180d14d8cea9967046
Mulunghisi NUKERI <mnukeri@c5r8s5.wethinkcode.co.za>
2019-06-25 10:44:01 +0200

    strsplit update

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,35 +6,53 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 12:41:00 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/24 18:39:13 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/25 10:43:27 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-char	**ft_strsplit(char const *str, char c)
+static int	ft_wc(const char *str, char c)
+{
+	int	p;
+	int	k;
+
+	p = 0;
+	k = 0;
+	if (str[0] == c)
+		p++;
+	while (str[p])
+	{
+		if (str[p] == c && str[p - 1] != c)
+			k++;
+		p++;
+	}
+	if (str[p] == '\0' && str[p - 1] != c)
+		k++;
+	return (k);
+}
+
+char		**ft_strsplit(char const *str, char c)
 {
 	int		p;
 	int		i;
 	int		j;
-	int		rows;
 	char	**y;
 
-	rows = ft_wc(str, c);
-	if (!(y = (char**)malloc(sizeof(char*) * rows)))
+	if (!(y = (char**)malloc(sizeof(char*) * ft_wc(str, c))))
 		return (NULL);
 	i = 0;
 	p = 0;
-	while (str[p] && i < rows)
+	while (str[p] && i < ft_wc(str, c))
 	{
 		while (str[p] == c && str[p])
 			p++;
-		y[i] = ft_memalloc(15);
 		j = 0;
 		while (str[p + j] != '\0' && str[p + j] != c)
 			j++;
-		y[i] = ft_strsub(str, p, j);
+		y[i] = ft_memalloc(j + 1);
+		y[i] = ft_strsub(str, p, (j + 1));
+		y[i][j] = '\0';
 		p = p + j;
 		i++;
 	}
