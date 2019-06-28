b23f4b6ca8ae7a11c6ce2e3192f7ce09d539d891
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-06-28 16:57:51 +0200

    strequ update

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 17:16:11 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/12 17:26:48 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/28 16:57:31 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int		ft_strequ(char const *s1, char const *s2)
 	int p;
 	int k;
 
+	if (s1 == NULL || s2 == NULL)
+		return (0);
 	p = 0;
 	while (s1[p] != '\0' && s2[p] != '\0' && s1[p] == s2[p])
 	{
