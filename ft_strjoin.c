f0f8204275f7f189d0200372b4fb0c762347b133
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-06-28 16:50:01 +0200

    strjoin update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 18:07:52 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/12 18:21:35 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/28 16:49:24 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		k;
 	char	*new;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	new = (char*)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (new == NULL)
+		return (NULL);
 	p = 0;
 	k = 0;
 	while (s1[p] != '\0')
@@ -29,11 +33,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	}
 	p = 0;
 	while (s2[p] != '\0')
-	{
-		new[k] = s2[p];
-		p++;
-		k++;
-	}
+		new[k++] = s2[p++];
 	new[k] = '\0';
 	return (new);
 }
