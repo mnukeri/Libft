ff54f37c053d167591d6f51275bbe6d021928634
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-06-28 16:39:51 +0200

    strtrim update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,35 +6,37 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 10:34:35 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/11 12:12:34 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/28 16:39:31 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strtrim(char const *s)
 {
 	int		p;
 	char	*new;
 	int		m;
+	int		k;
+	int		n;
 
-	new = (char*)malloc(ft_strlen(s) * sizeof(char));
-	if (new == NULL)
+	if (s == NULL)
 		return (NULL);
 	p = 0;
-	m = 0;
-	while (ft_isprint(s[p]) == 0 || s[p] == ' ')
-	{
-		if (s[p] == ' ' || s[p] == '\t' || s[p] == '\n')
-			p++;
-	}
-	while (ft_isprint(s[p]) == 1)
-	{
-		new[m] = s[p];
+	while (s[p] == '\t' || s[p] == ' ' || s[p] == '\n')
 		p++;
-		m++;
-	}
-	m--;
+	n = p;
+	m = ft_strlen(s) - 1;
+	if (m > p)
+		while (s[m] == ' ' || s[m] == '\t' || s[m] == '\n')
+			m--;
+	k = m + 1 - p;
+	if (!(new = (char*)malloc(sizeof(char) * (k + 1))))
+		return (NULL);
+	m = 0;
+	while (m < k && s[p])
+		new[m++] = s[p++];
 	new[m] = '\0';
 	return (new);
 }
