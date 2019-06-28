fffdf84511395e403bccc40adfaa6741f044fa13
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-06-28 16:52:17 +0200

    strsub update

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 17:41:29 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/12 17:56:27 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/28 16:51:57 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,16 @@ char	*ft_strsub(char const *str, unsigned int start, size_t len)
 	size_t	k;
 	char	*new;
 
+	if (str == NULL)
+		return (NULL);
 	p = 0;
 	while (str[p] != '\0' && p < start)
 		p++;
-	new = (char*)malloc((len + 1) * sizeof(char));
-	if (new == NULL)
+	if (!(new = (char*)malloc((len + 1) * sizeof(char))))
 		return (NULL);
 	k = 0;
 	while (str[p] != '\0' && k < len)
-	{
-		new[k] = str[p];
-		p++;
-		k++;
-	}
+		new[k++] = str[p++];
 	new[k] = '\0';
 	return (new);
 }
