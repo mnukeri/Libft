945d472e687f4758a6c35ca9f74330c365a10c7c
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-06-28 21:54:54 +0200

    strnew update

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 12:40:16 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/11 13:20:29 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/28 21:53:32 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,12 @@ char	*ft_strnew(size_t size)
 	size_t	p;
 	char	*new;
 
-	new = (char*)malloc(size * sizeof(char));
+	new = (char*)malloc((size + 1) * sizeof(char));
 	if (new == NULL)
 		return (NULL);
 	p = 0;
 	while (p < size)
-	{
-		new[p] = '\0';
-		p++;
-	}
+		new[p++] = '\0';
 	new[p] = '\0';
 	return (*(&new));
 }
