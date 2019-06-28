49b00ac49e3d44cc20bc4d477fdbff80d7806f15
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-06-28 16:55:11 +0200

    strnequ update

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 17:28:24 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/12 17:38:09 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/28 16:54:51 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t k;
 	size_t p;
 
+	if (s1 == NULL || s2 == NULL)
+		return (0);
 	k = 0;
 	while (s1[k] != '\0' && s2[k] != '\0' && s1[k] == s2[k] && k < n)
 		k++;
