1f64ab3815cbf51afa58f4c93779d79ddf6814d7
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-06-29 14:44:32 +0200

    strdup update

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 16:53:22 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/05/24 16:53:29 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/29 14:44:13 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,14 @@ char	*ft_strdup(const char *s1)
 	{
 		len++;
 	}
-	str = (char*)malloc(sizeof(*str) * (len + 1));
+	if (!(str = (char*)malloc(sizeof(*str) * (len + 1))))
+		return (NULL);
 	p = 0;
 	while (p < len)
 	{
 		str[p] = s1[p];
 		p++;
 	}
+	str[p] = '\0';
 	return (str);
 }
