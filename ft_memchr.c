550b113d5bf12c7a74ec32d88831cbeb1428f122
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-06-30 14:53:50 +0200

    memchr update

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 16:14:07 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/05 16:26:21 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/06/30 14:53:22 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@ void	*ft_memchr(const void *str, int c, size_t n)
 {
 	size_t			k;
 	unsigned char	*st;
+	size_t			m;
 
 	st = (unsigned char *)str;
 	k = 0;
-	while (k < n)
+	while (k < n && st[k])
 	{
-		if (st[k] == c)
-		{
-			return ((char*)&str[k]);
-		}
+		if (st[k] == (unsigned char)c)
+			return ((char *)&st[k]);
 		else
-		{
 			k++;
-		}
 	}
+	m = ft_strlen(str);
+	if (c == '\0')
+		return ((char *)&st[m]);
 	return (0);
 }
