9f1fc2d47253044515e4c6f0e3fb91980eaa54b1
Mulunghisi NUKERI <mnukeri@c5r8s1.wethinkcode.co.za>
2019-07-01 13:13:36 +0200

    strncpy update

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 16:32:16 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/06/29 15:45:44 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/07/01 13:12:49 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ char	*ft_strncpy(char *dest, const char *src, size_t len)
 	size_t p;
 	size_t k;
 
+	if (!(src))
+		return (0);
 	if (len == 0)
 		return (0);
+	ft_bzero(dest, len);
 	p = 0;
 	k = 0;
-	while (src[p] != '\0' && k < len)
+	while (src[p] && k < len)
 	{
 		dest[p] = src[p];
 		p++;
