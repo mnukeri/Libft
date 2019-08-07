2430f69793df57fb7972e6780cb24fd245deed30
Mulunghisi NUKERI <mnukeri@c5r8s10.wethinkcode.co.za>
2019-08-07 17:13:49 +0200

    -1, 0, 1 update

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 14:28:48 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/05/25 14:52:32 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/08/07 17:10:28 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,9 @@ int		ft_strcmp(const char *s1, const char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
-	if (ft_isalpha(s1[i]) == 1 && ft_isalpha(s2[i]) == 1)
-	{
-		if (ft_iscaps(s1[i]) == 1 && ft_iscaps(s2[i]) != 1)
-			return (1);
-	}
 	if (s1[i] < s2[i])
 		return (-1);
 	if (s1[i] > s2[i])
 		return (1);
-	return (0);
-}
-int	main()
-{
-	char s[] = "libft-U";
-	char st[] = "libft-u";
-
-	ft_putnbr(ft_strcmp(s, st));
-	ft_putchar('\n');
 	return (0);
 }
