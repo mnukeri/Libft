6cfc9d40aea024665b9ba1d109a69c5dc8a46458
Mulunghisi NUKERI <mnukeri@c5r8s10.wethinkcode.co.za>
2019-08-20 18:32:31 +0200

    func for first memory blocks in 2d array

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mnukeri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 14:02:38 by mnukeri           #+#    #+#             */
-/*   Updated: 2019/08/18 13:47:12 by mnukeri          ###   ########.fr       */
+/*   Updated: 2019/08/20 18:31:23 by mnukeri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,9 +70,8 @@ char	*ft_strmap(char const *s, char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_itoa(int n);
 void	ft_memdel(void **ap);
-void	ft_char_swap(char *a, char *b);
+void	ft_swap(char *a, char *b);
 int		ft_iscaps(int c);
-void	ft_int_swap(int *a, int *b);
-int		ft_wc(const char *str, char c);
+void	**ft_memalloc_2d(size_t size);
 
 #endif
